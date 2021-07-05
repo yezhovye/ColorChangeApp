a9e01dc07da6dfc52284bd63586b7d9058189853
Sawwaty <yezhovye@mac.com>
2021-07-05 09:52:31 +0300

    UI elements are added, connections are established

--- ColorChangeApp/ViewController.swift
+++ ColorChangeApp/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
